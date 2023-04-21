1f15b2332ee14730e2d7eaef38b3f4424985354e
Jean Pierre <jeanlondono09@gmail.com>
2023-04-21 14:50:51 -0500

    first

---
+++ colombia.js
@@ -0,0 +1,11 @@
+let map = L.map('map').setView([6.263980288403682, -75.57591350561498],6)
+
+//Agregar tilelAyer mapa base desde openstreetmap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+document.getElementById('select-location').addEventListener('change',function(e){
+  let coords = e.target.value.split(",");
+  map.flyTo(coords,13);
+})
